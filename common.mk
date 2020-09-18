57cc1d4961671727cd0dd2cd7e9df8b1c1a5abc9
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    common: Build libhidltransport/libhwbinder
     * required by some blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- common.mk
+++ common.mk
@@ -194,5 +194,7 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0_system \
+    libhidltransport.vendor \
+    libhwbinder.vendor
 endif
