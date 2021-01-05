333eddd30c0746b74cade9d401ee70d9955d6e19
Christian Oder <myself5@carbonrom.org>
2021-01-05 12:31:37 -0300

    common: permissions: Add basic hotword permissions
     * Required for older hotword blobs
    
    Change-Id: I3a978ddda791a00274064e47348953c46006ace2
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- common.mk
+++ common.mk
@@ -195,6 +195,7 @@ PRODUCT_COPY_FILES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     $(QCOM_COMMON_PATH)/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    $(QCOM_COMMON_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
     $(QCOM_COMMON_PATH)/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # HIDL
