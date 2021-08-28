0c45237bba4436e236a8b1e01dcddee65121d299
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-08-29 04:20:06 +0530

    common: Import vendor FCM from core-utils

--- common.mk
+++ common.mk
@@ -154,7 +154,7 @@ ifeq ($(call is-board-platform-in-list,$(QCOM_BOARD_PLATFORMS)),true)
 ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
 # Compatibility matrix
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
+    $(QCOM_COMMON_PATH)/vendor_framework_compatibility_matrix.xml
 endif
 
 # Components
