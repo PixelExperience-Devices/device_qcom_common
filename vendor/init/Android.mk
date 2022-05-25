584d7d7f4033e6ffd50701ed7b6c1141a560db22
Alexander Koskovich <akoskovich@pm.me>
2022-05-25 15:27:44 -0700

    common: init: Move init.recovery.qcom.rc to Android.bp
     * /system/etc/init is automatically parsed in recovery.
    
    Change-Id: I7914a9191c5a2ba13b1d60d59fd8b95436d131c3
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor/init/Android.mk
+++ vendor/init/Android.mk
@@ -17,13 +17,6 @@ ifneq (,$(filter init, $(TARGET_COMMON_QTI_COMPONENTS)))
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.qcom.rc
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
