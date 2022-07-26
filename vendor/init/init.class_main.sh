375e373a6bc8c82f18310d7a49bc84f452360735
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-07-27 04:52:08 +0530

    Revert "vendor: init: Stop all qcrilds on APQ target"
    This reverts commit 431c1210cec42958063648988958d4545ed96a7c.

--- vendor/init/init.class_main.sh
+++ vendor/init/init.class_main.sh
@@ -40,8 +40,6 @@ case "$baseband" in
     setprop ro.vendor.radio.noril yes
     stop vendor.ril-daemon
     stop vendor.qcrild
-    stop vendor.qcrild2
-    stop vendor.qcrild3
 esac
 
 case "$baseband" in
