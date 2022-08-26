ce286535a753fc7a8d31ed92d7b2ba0da29d30d1
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-08-26 02:03:01 +0000

    Revert "Revert "common: init: Move ueventd.qcom.rc to /vendor/etc.""
    This reverts commit 4427ae3690521205e5c5040f2e4af7fd0e571e30.

--- vendor/init/Android.mk
+++ vendor/init/Android.mk
@@ -16,12 +16,4 @@ ifneq (,$(filter init, $(TARGET_COMMON_QTI_COMPONENTS)))
 
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.qcom.rc
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
-LOCAL_MODULE_STEM  := ueventd.rc
-LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
 endif
