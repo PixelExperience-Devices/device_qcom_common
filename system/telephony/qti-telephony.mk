7807df228d1f42e9ebaddd08399bd49e2126f723
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-12-26 16:13:06 +0000

    system: telephony: Add framework manifest to hidl AtCmdFwdService
    [1] - This was removed because system atcmdfwd-saidl was added in QSSI 13.0. But because of GRF, vendor hidl manifest must be set to R and S vendor blobs
    [2] - I hwservicemanager: getTransport: Cannot find entry vendor.qti.hardware.radio.atcmdfwd@1.0::IAtCmdFwd/AtCmdFwdService in either framework or device VINTF manifest.
          E HidlServiceManagement: Service vendor.qti.hardware.radio.atcmdfwd@1.0::IAtCmdFwd/AtCmdFwdService must be in VINTF manifest in order to register/get.
    [3] - 12-26 12:13:28.995 W/AutoPasteSuggestionHelper(3603): AutoPasteSuggestionHelper.createProactiveSuggestions():377 Failed to create item chips. Clip items are [ClipItem{ id = 1672071207744, timestamp = 1672071207744, clipItemContent = ClipItemContent{text=I hwservicemanager: getTransport: Cannot find entry vendor.qti.hardware.radio.atcmdfwd@1.0::IAtCmdFwd/AtCmdFwdService in either framework or device VINTF manifest.
    [4] - 12-26 12:13:28.995 W/AutoPasteSuggestionHelper(3603): E HidlServiceManagement: Service vendor.qti.hardware.radio.atcmdfwd@1.0::IAtCmdFwd/AtCmdFwdService must be in VINTF manifest in order to register/get., htmlText=, itemType=0, entityType=0, uri=, groupId=-1, viewType=0}}].
    [5] - 01-04 11:36:04.485 E/AtFwdService(2681):  at vendor.qti.hardware.radio.atcmdfwd.V1_0.IAtCmdFwd$Stub.registerAsService(IAtCmdFwd.java:524)
    [6] - 01-04 11:36:04.484 E/HidlServiceManagement(2681): Service vendor.qti.hardware.radio.atcmdfwd@1.0::IAtCmdFwd/AtCmdFwdService must be in VINTF manifest in order to register/get.
    
    Change-Id: Iaee3ef28f9c62c22de41f7d3310cf91709101527
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- system/telephony/qti-telephony.mk
+++ system/telephony/qti-telephony.mk
@@ -16,7 +16,8 @@
 PRODUCT_COPY_FILES += \
     $(QCOM_COMMON_PATH)/system/telephony/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 
-# HIDL
+# AIDL / HIDL
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/system/telephony/framework_manifest.xml
 SYSTEM_EXT_MANIFEST_FILES += $(QCOM_COMMON_PATH)/system/telephony/atcmdfwd-saidl.xml
 
 # Packages
