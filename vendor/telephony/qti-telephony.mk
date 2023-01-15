92b73c191a30183ac8bc5e1fc05679a46a8e57e4
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-01-15 13:06:22 +0000

    vendor: telephony: Skip radio rat on property only for 5.10 family
    [1] - 8f946a577ff8bbcddc8b2b9178f21d9233aca216
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor/telephony/qti-telephony.mk
+++ vendor/telephony/qti-telephony.mk
@@ -39,6 +39,11 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi.hostapd@1.0.vendor \
     android.system.net.netd@1.1.vendor
 
+ifneq (,$(filter 3.18 4.4 4.9 4.14 4.19 5.4, $(TARGET_KERNEL_VERSION)))
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.radio.rat_on=combine
+endif
+
 PRODUCT_VENDOR_PROPERTIES += \
     persist.radio.multisim.config=dsds \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
