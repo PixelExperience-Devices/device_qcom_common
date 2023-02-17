3802e110b09081fc259af9438096801e8c39cd4b
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-02-17 12:23:44 -0800

    common: Defin vendor/aosp FCM
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- common.mk
+++ common.mk
@@ -148,7 +148,8 @@ DEVICE_MATRIX_FILE += \
     device/qcom/vendor-common/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
+    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml \
+    vendor/aosp/config/device_framework_matrix.xml
 
 PRODUCT_VENDOR_PROPERTIES += ro.vendor.qti.va_aosp.support=1
 PRODUCT_ODM_PROPERTIES += ro.vendor.qti.va_odm.support=1
