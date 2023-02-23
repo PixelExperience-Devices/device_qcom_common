0ed636d7ff5821cfb7f4daee52cdd5b3b5b917e8
Jake Weinstein <jake@aospa.co>
2023-02-24 02:26:49 +0900

    vendor: Introduce wlan-legacy component
    LA.VENDOR.1.0 cnss-daemon breaks Wi-Fi on older
    SoCs.
    
    Change-Id: I8e50fe07983381a52297e5a7e6b384662e43bd82
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor/wlan-legacy/Android.mk
+++ vendor/wlan-legacy/Android.mk
@@ -14,16 +14,16 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_WLAN_COMPONENT_VARIANT),wlan)
+ifeq ($(TARGET_WLAN_COMPONENT_VARIANT),wlan-legacy)
 
 include $(CLEAR_VARS)
 
 ifeq ($(call is-board-platform-in-list,msm8998 sdm660),true)
-include device/qcom/wlan/sdm660_64/AndroidBoardWlan.mk
+-include device/qcom/wlan/sdm660_64/AndroidBoardWlan.mk
 else ifeq ($(call is-board-platform-in-list,sm6150),true)
-include device/qcom/wlan/talos/AndroidBoardWlan.mk
+-include device/qcom/wlan/talos/AndroidBoardWlan.mk
 else
-include device/qcom/wlan/$(TARGET_BOARD_PLATFORM)/AndroidBoardWlan.mk
+-include device/qcom/wlan/$(TARGET_BOARD_PLATFORM)/AndroidBoardWlan.mk
 endif
 
 endif
