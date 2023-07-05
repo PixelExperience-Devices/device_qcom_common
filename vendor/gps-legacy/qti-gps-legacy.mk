faef91aefc0d0d2b241e8dac398269f1dee0d30a
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-07-05 16:57:05 +0000

    vendor: gps-legacy: Fix inherit GPS HAL makefile
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor/gps-legacy/qti-gps-legacy.mk
+++ vendor/gps-legacy/qti-gps-legacy.mk
@@ -22,7 +22,11 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 4.0
 
 # Inherit the GPS HAL.
-$(call inherit-product-if-exists, hardware/qcom/gps/gps_vendor_product.mk)
+ifeq ($(TARGET_USE_SM8150_HALS),true)
+$(call inherit-product-if-exists, hardware/qcom-caf/sm8150-common/gps/gps_vendor_product.mk)
+else ifeq ($(TARGET_USE_BENGAL_HALS),true)
+$(call inherit-product-if-exists, hardware/qcom-caf/bengal/gps/gps_vendor_product.mk)
+endif
 
 # Overlays
 PRODUCT_PACKAGES += \
