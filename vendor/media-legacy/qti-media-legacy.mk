e87bd14785477f06bc91e13590aff89c17fba349
Immanuel Raj <iamimmanuelraj@gmail.com>
2023-09-19 22:41:47 +0530

    vendor: media-legacy: Build android.hardware.media.c2 1.2 vendor interface

--- vendor/media-legacy/qti-media-legacy.mk
+++ vendor/media-legacy/qti-media-legacy.mk
@@ -45,6 +45,7 @@ PRODUCT_COPY_FILES += \
 
 # Packages
 PRODUCT_PACKAGES += \
+    android.hardware.media.c2@1.2.vendor \
     libavservices_minijail.vendor \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
